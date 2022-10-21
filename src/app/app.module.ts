4a60bdf47de40cf74430e273967ed9f48af62ed8
emendz <emendezgonz@gmail.com>
2022-10-21 17:38:08 -0400

    ultimos cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import { ToastrModule } from 'ngx-toastr';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +13,6 @@ import { FooterComponent } from './components/footer/footer.component';
 import { FormComponent } from './components/form/form.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ScanQrComponent } from './components/scan-qr/scan-qr.component';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,6 +32,9 @@ import { ScanQrComponent } from './components/scan-qr/scan-qr.component';
     }),
     ZXingScannerModule,
     HttpClientModule,
+    ReactiveFormsModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import * as data from '../../json/data.json';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { HttpClientService } from 'src/app/service/http-client.service';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
@@ -8,9 +18,83 @@ import * as data from '../../json/data.json';
 })
 export class FormComponent implements OnInit {
   dataJson: any;
-  constructor() {}
+  route: string = 'Factura/ByCodigoDetalle';
+  form: FormGroup;
+  ticket: FormArray;
+  errorMessage: string = null;
+
+  constructor(
+    private _httpClient: HttpClientService,
+    private _route: ActivatedRoute,
+    private _fb: FormBuilder,
+    private toastr: ToastrService,
+    private _router: Router
+  ) {}
+
+  get tickets() {
+    return this.form.get('tickets') as FormArray;
+  }
 
   ngOnInit(): void {
-    this.dataJson = data;
+    this._route.paramMap.subscribe((params) => {
+      let codigo = params.get('codigo');
+      if (codigo) {
+        this._httpClient
+          .get(`${this.route}?codigo=${codigo}`)
+          .subscribe((res) => {
+            if (res.statusCode == 200) {
+              this.dataJson = res.data;
+              this.buildForm(this.dataJson);
+            } else {
+              this.errorMessage = res.message;
+              console.log(this.errorMessage);
+            }
+          });
+      }
+    });
+  }
+
+  buildForm(model: any = {}) {
+    this.form = this._fb.group({
+      tickets: new FormArray([]),
+    });
+    this.ticket = this.form.get('tickets') as FormArray;
+    if (model?.facturaDetalles[0]) {
+      for (let item of model?.facturaDetalles[0].tickets) {
+        this.ticket?.push(this.generateNewRow(item));
+      }
+    }
+  }
+
+  generateNewRow(model: any = {}): FormGroup {
+    return new FormGroup({
+      ticketId: new FormControl(model.id, Validators.required),
+      cintilloNumber: new FormControl(Validators.required),
+    });
+  }
+
+  submitForm() {
+    swal
+      .fire({
+        title: 'Esta acción no podrá ser revertida',
+        text: 'Seguro que desea registrar estos cintillos y dar entrada a estos tickets?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Aceptar',
+        cancelButtonText: 'Cancelar',
+        confirmButtonColor: '#002d68',
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          this._httpClient
+            .post(this.form.value, 'Ticket/ActivateTicket')
+            .subscribe((res) => {
+              if (res.statusCode == 201) {
+                this.toastr.success('Registrado correctamente', 'Confirmado');
+                this._router.navigate([`scan/`]);
+              }
+            });
+        }
+      });
   }
 }
